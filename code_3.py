fa70404c3602b16c3717b5b8806ef23f45700552
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-09-22 23:17:01 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,56 +1,42 @@
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+}
+        }
 
-    data = generate_random_data()
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
 
+    }
+        }
+public class RandomDataGenerator {
+        for (int item : data) {
+        int[] data = new int[10];        print(f"Random Number: {item}")
 import random
-        print(f"Random Number: {item}")
-    return data
+    for item in data:
 
-def main():
 
-    main()
-if __name__ == "__main__":import random
-
-def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def main():
-    return data
 if __name__ == "__main__":
 
-    for item in data:
-
-
     main()
-        print(f"Random Number: {item}")    data.forEach(item => {
+def generate_random_data():
+    data = generate_random_data()
+def main():
 
-        console.log("Random Number: " + item);
-    }
-}
-}
+    return data        console.log("Random Number: " + item);
     const data = [];
+}
+    }
+
 
 main();
+function main() {
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+function generateRandomData() {
+
+}
     });
     for (let i = 0; i < 10; i++) {
     return data;
-
-    const data = generateRandomData();
-function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {import random
-
-
-
-def generate_random_data():
-    for item in data:
-    data = generate_random_data()
-def main():
-
-    return data
-if __name__ == "__main__":
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
